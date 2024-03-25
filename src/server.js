f2c25854cd8c1ebfc2b4c6a374fa697a939d7f14
= <=>
2024-03-25 17:15:27 +0700

    build prj

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import initWebRoutes from "./routes/web";
+import configViewEngine from "./config/viewEngine";
+require("dotenv").config();
+
+import bodyParser from "body-parser";
+//để lấy các tham số bên Client gửi lên: query, params, body
+
+let app = express();
+
+//config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//config view engine
+configViewEngine(app);
+
+//init all web routes
+initWebRoutes(app);
+
+let PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Backend is running on port ${PORT}`);
+});
