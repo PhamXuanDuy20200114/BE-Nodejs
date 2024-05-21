1e3e08481bff99be4a564f010b98501dd678cece
= <=>
2024-05-21 23:30:03 +0700

    vid 67

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -16,7 +16,36 @@ const getTopDoctorHome = async (req, res) => {
 
 }
 
+const getAllDoctors = async (req, res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors);
+    } catch (e) {
+        console.log('error:', e);
+        return res.status(200).json({
+            errCode: -1,
+            message: 'Error from server'
+        });
+    }
+}
+
+const postInfoDoctor = async (req, res) => {
+    try {
+        let response = await doctorService.saveDetailInfoDoctor(req.body);
+        return res.status(200).json(response);
+    } catch (e) {
+        console.log('error:', e);
+        return res.status(200).json({
+            errCode: -1,
+            message: 'Error from server'
+        });
+
+    }
+}
+
 
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome
+    getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    postInfoDoctor: postInfoDoctor
 }
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,5 +1,6 @@
 import { raw } from 'body-parser';
 import db from '../models/index';
+import { where } from 'sequelize';
 
 let getTopDoctorHome = (limitInput) => {
     return new Promise(async (resolve, reject) => {
@@ -29,6 +30,58 @@ let getTopDoctorHome = (limitInput) => {
     })
 }
 
+const getAllDoctors = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            console.log('Check here')
+            let data = await db.User.findAll({
+                where: { roleId: 'R2' },
+                attributes: {
+                    exclude: ['password', 'image']
+                },
+            })
+            resolve({
+                errCode: 0,
+                data: data
+            });
+        } catch (e) {
+            console.log('error:', e);
+            reject(e);
+        }
+    })
+}
+
+const saveDetailInfoDoctor = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.doctorId || !data.contentHTML || !data.contentMarkdown) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters'
+                })
+            }
+            else {
+                await db.Markdown.create({
+                    doctorId: data.doctorId,
+                    contentHTML: data.contentHTML,
+                    contentMarkdown: data.contentMarkdown,
+                    description: data.description ? data.description : '',
+                })
+                resolve({
+                    errCode: 0,
+                    message: 'Save info doctor success'
+                })
+            }
+
+        } catch (e) {
+            console.log('error:', e);
+            reject(e);
+        }
+    });
+}
+
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome
+    getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    saveDetailInfoDoctor: saveDetailInfoDoctor
 }
